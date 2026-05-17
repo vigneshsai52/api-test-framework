a7db0ecce4924c2131afe2ed39d70c65ef769adc
vignesh <vigneshsai52@gmail.com>
2026-05-17 11:53:56 +0530

    Initial commit

---
+++ utils/api_client.py
@@ -0,0 +1,15 @@
+import requests
+
+BASE_URL = "https://reqres.in/api"
+
+def get(endpoint, params=None):
+    return requests.get(f"{BASE_URL}{endpoint}", params=params)
+
+def post(endpoint, payload):
+    return requests.post(f"{BASE_URL}{endpoint}", json=payload)
+
+def put(endpoint, payload):
+    return requests.put(f"{BASE_URL}{endpoint}", json=payload)
+
+def delete(endpoint):
+    return requests.delete(f"{BASE_URL}{endpoint}")
